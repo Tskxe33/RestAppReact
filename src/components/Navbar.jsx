409a06c52fb3b05a08af5f8cc61049d22e684b4f
Petro Arva <petroarva33@gmail.com>
2021-11-09 10:04:37 +0100

    Created navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
+import logoImage from "../images/logoImage.png";
+import NavBarButtons from "./NavbarButtons";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <Fragment>
+        <header className="header">
+          <nav className="nav">
+            <div className="nav__navigation">
+              <ul className="nav__list">
+                <li className="nav__items">
+                  <Link to="home.html" className="nav__links">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav__items">
+                  <Link to="index.html" className="nav__links">
+                    Find Restaurants
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="nav__logo">
+              <img
+                src={logoImage}
+                className="logo animate__animated animate__tada"
+              />
+            </div>
+            <NavBarButtons />
+          </nav>
+        </header>
+      </Fragment>
+    );
+  }
+}
+
+export default NavBar;
